ccc837a66ce966768774413e686858ebe8e0793e
Dora0 <43771714+Dora0@users.noreply.github.com>
2018-10-03 16:38:02 +0900

    Add files via upload

---
+++ src/main/java/com/example/demo/Chatbot2Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chatbot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chatbot2Application.class, args);
+	}
+}
